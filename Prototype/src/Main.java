00f1adf1ead3d19122614f9ff4659339871d9ad6
mmaroun <miguel.maroun@softeam.fr>
2024-03-12 10:01:41 +0100

    Prototype design pattern

---
+++ Prototype/src/Main.java
@@ -0,0 +1,28 @@
+import org.apache.commons.lang3.SerializationUtils;
+
+import java.io.Serializable;
+import model.Foo;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Employee john = new Employee("John",
+//                new Address("123 London Road", "London", "UK"));
+//
+//        //Employee chris = john;
+//        Employee chris = new Employee(john);
+//
+//        chris.name = "Chris";
+//        System.out.println(john);
+//        System.out.println(chris);
+//        Foo foo = new Foo(42, "life");
+//        // use apache commons!
+//        Foo foo2 = SerializationUtils.roundtrip(foo);
+//
+//        foo2.whatever = "xyz";
+//
+//        System.out.println(foo);
+//        System.out.println(foo2);
+    }
+
+}
