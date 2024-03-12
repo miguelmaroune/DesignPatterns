390e583012b239a321973dd10357968ee32957b0
mmaroun <miguel.maroun@softeam.fr>
2024-03-12 16:34:32 +0100

    Bridge design pattern

--- Bridge.Structural/src/Main.java
+++ Bridge.Structural/src/Main.java
@@ -1,8 +1,21 @@
+import abstractions.AmericanRestaurant;
+import abstractions.ItalianRestaurant;
+import abstractions.Restaurant;
+import implementations.PepperoniPizza;
+import implementations.VeggiePizza;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Restaurant americanRestaurant = new AmericanRestaurant(new PepperoniPizza());
+        americanRestaurant.deliver();
+
+        System.out.println("==========================================");
+
+        Restaurant italianRestaurant = new ItalianRestaurant(new VeggiePizza());
+        italianRestaurant.deliver();
+
+
     }
 
 }
